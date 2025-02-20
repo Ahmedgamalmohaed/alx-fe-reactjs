2250df74c8731975713a65fb7b9d018c671d3a9f
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-20 13:23:42 +0200

    Update UserProfile.jsx

--- alx-react-app/src/components/UserProfile.jsx
+++ alx-react-app/src/components/UserProfile.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
 
+const UserProfile = () => {
+  return (
+    <div>
+      <h1>User Profile</h1>
+      <p>This is the user profile component.</p>
+    </div>
+  );
+};
+
+export default UserProfile;
