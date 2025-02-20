8ece0b2dabb2022848d6dd9dcbbda5f42946e33c
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-20 15:07:32 +0200

    Create App.jsx

---
+++ alx-react-app-new/src/components/App.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import UserProfile from './components/UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
+import ProfilePage from './ProfilePage'; // Import ProfilePage
+import { UserProvider } from './components/UserContext'; // Import UserProvider
+
+const userData = {
+  name: 'John Doe',
+  email: 'john.doe@example.com'
+};
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserProvider userData={userData}>
+          <Header />
+          <MainContent />
+          <ProfilePage /> {/* Wrap ProfilePage inside UserProvider */}
+          <UserProfile />
+          <Footer />
+        </UserProvider>
+      </header>
+    </div>
+  );
+}
+
+export default App;
