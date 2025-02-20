f5939e08b07faddf58f5fa05332d8576a790e42a
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-20 13:03:36 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,1 +1,14 @@
+import React from 'react';
+import WelcomeMessage from './components/WelcomeMessage';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <WelcomeMessage />
+      </header>
+    </div>
+  );
+}
+
+export default App;
