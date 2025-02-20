a9ad8b2046152c5f94b6a3654cbd9dfc9f637685
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-20 13:33:08 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,16 +1,20 @@
 import React from 'react';
 import UserProfile from './components/UserProfile';
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <Header />
+        <MainContent />
         <UserProfile />
+        <Footer />
       </header>
     </div>
   );
 }
+
 export default App;
-import Header from './Header';,
-import MainContent from './MainContent';, and
-import Footer from './Footer';.
