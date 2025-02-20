5d4d5f19a01c37ce964f764018999e85de16d846
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-20 13:25:42 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,1 +1,13 @@
+import React from 'react';
+import UserProfile from './components/UserProfile';
 
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <UserProfile />
+      </header>
+    </div>
+  );
+}
+export default App;
