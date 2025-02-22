4857eab4613f1663632099d3cebd3b4f713f0ce3
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-22 13:24:42 +0200

    Update App.jsx

--- alx-react-app-new/src/components/App.jsx
+++ alx-react-app-new/src/components/App.jsx
@@ -29,15 +29,23 @@ function App() {
 }
 function App() {
 export default App;
-  import React from 'react';
-import UserProfile from './components/UserProfile';
+import React from 'react';
+import PropTypes from 'prop-types';
 
-function App() {
+const UserProfile = ({ name, age, bio }) => {
   return (
-    <div className="App">
-      <UserProfile name="Alice" age={25} bio="Loves hiking and photography" />
+    <div>
+      <h1>{name}'s Profile</h1>
+      <p>Age: {age}</p>
+      <p>Bio: {bio}</p>
     </div>
   );
-}
+};
 
-export default App;
+UserProfile.propTypes = {
+  name: PropTypes.string.isRequired,
+  age: PropTypes.number.isRequired,
+  bio: PropTypes.string.isRequired,
+};
+
+export default UserProfile;  
