8e293579162736e915b5a602e1371ba24a90ea95
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-23 17:18:23 +0200

    Update App.jsx

--- alx-react-app/src/components/App.jsx
+++ alx-react-app/src/components/App.jsx
@@ -1,2 +1,3 @@
 ["WelcomeMessage", "<WelcomeMessage />"]
 <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
+src/components/UserProfile.jsx doesn't contain: ["{props.name}", "{props.age}", "Age", "{props.bio}", "Bio"]
