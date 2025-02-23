866a2a2931fa46b101d50ddb1238b1c162a477c6
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-23 17:29:36 +0200

    Update App.jsx

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,11 +1,6 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import WelcomeMessage from './components/WelcomeMessage';
-import Header from './components/Header';
-import MainContent from './components/MainContent';
-import Footer from './components/Footer';
-import UserProfile from './components/UserProfile';
+import viteLogo from '/vite.svg
 import './App.css'
 
 function App() {
