8b6fb005eacb8ebdb896807c09ffa6baf9c6d33f
Ahmed gamal <144253822+Ahmedgamalmohaed@users.noreply.github.com>
2025-02-23 20:44:23 +0200

    Update App.jsx

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -2,6 +2,8 @@ import ProfilePage from './ProfilePage';
 
 function App() {
   const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
+  ["UserContext", "UserContext.Provider", "value"]
+  src/App.jsx doesn't contain: ["UserContext", "UserContext.Provider", "value"]
 
   return <ProfilePage userData={userData} />;
 }
